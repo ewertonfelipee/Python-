3ee4bdc9e3b8d7e040d6660b0de00d15bd6a4b29
Ewerton Felipe Silva Marinho <ewertonfmarinho@protonmail.ch>
2021-05-18 21:15:40 -0300

    media ponderada

---
+++ exercicios/uri1006.py
@@ -0,0 +1,11 @@
+#Fazer a media
+
+def media():
+    
+    a = float(input())
+    b = float(input())
+    c = float(input())
+    m = ((a*2)+(b*3)+(c*5))/10
+    print("MEDIA = {0:1.1f}".format(m))
+    
+media()
