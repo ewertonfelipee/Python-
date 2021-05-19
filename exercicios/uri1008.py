8c77097ab55f86eb9f5288ce92d4ff70c0d2432e
Ewerton Felipe Silva Marinho <ewertonfmarinho@protonmail.ch>
2021-05-18 21:50:16 -0300

    employee salary

---
+++ exercicios/uri1008.py
@@ -0,0 +1,16 @@
+
+def salaryy():
+    
+    number = int(input())
+    
+    time = int(input())
+    
+    value = float(input())
+    
+    salary = time * value
+    
+    print("NUMBER =", number)
+    
+    print("SALARY = U$ {0:2.2f}".format(salary))
+
+salaryy()
