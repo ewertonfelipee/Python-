a903047c4dadfe2cf8bc794e545ce6d62cbce763
Ewerton Felipe Silva Marinho <ewertonfmarinho@protonmail.ch>
2021-06-29 20:44:16 -0300

    Chapter 2 Exercises

---
+++ exercicios/Chap2Exer13.py
@@ -0,0 +1,18 @@
+# CHAPTER 2 EXERCISE NO. 12345
+
+def exr_thirteen():
+    
+    for i in range(1, 11, 1):
+        print(i)
+            
+exr_thirteen()
+
+print(" ")
+
+def exr_thirteen2():
+    j = 1
+    while j <= 10:
+        print(j)
+        j += 1
+        
+exr_thirteen2()
---
+++ exercicios/Chap2Exer9.py
@@ -0,0 +1,14 @@
+# CHAPTER 2 EXERCISE NO. 9
+
+def conditional(spam):
+    
+    if(spam == 1):
+        print("Hello")
+    elif(spam == 2):
+        print("Howdy")
+    elif(spam == 3):
+        print("Greetings!")
+    else:
+        print("Enter with numbers between 1 and 3")
+
+conditional(spam = int(input()))
