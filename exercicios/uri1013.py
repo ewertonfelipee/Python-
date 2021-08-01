fb9716ac695cd5482ade8cbf053be27d645f4aad
ewertonfelipee <53940331+ewertonfelipee@users.noreply.github.com>
2021-08-01 13:29:12 -0300

    Update uri1013.py

--- exercicios/uri1013.py
+++ exercicios/uri1013.py
@@ -1,9 +1,9 @@
 # greater number
 
 def greater():
-    
+    # Below line read inputs from user using map() function
     g = list(map(int, input().split()))
-    
+    # max() function take the greater number in a sequence
     greaternumber = max(g)
     
     print(greaternumber, 'eh o maior')
