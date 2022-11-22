4babc42481c42e8cfcd90250c5a0a4172afa7434
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2022-11-21 22:41:57 -0300

    f-strings and main strings methods

---
+++ Concepts/Strings/f-Strings.py
@@ -0,0 +1,9 @@
+title = 'monitor gamer'
+inches = 32
+price = 1999.00
+
+print(
+    f'Product: {title.capitalize():*^25}\n'
+    f'Size: {inches}\n'
+    f'Price: {price:.2f}'
+)
---
+++ Concepts/Strings/methodsStrings.py
@@ -0,0 +1,8 @@
+print('     strip       '.strip())
+print('     strip       ')
+print('T,exto, c,o,m va,ri,a,s v,ir,gulas'.replace(',', ''))
+print('T,exto, c,o,m va,ri,a,s v,ir,gulas')
+print('eeeeeeeeee'.count('e'))
+print(' Texto Centralizado '.center(50, "-"))
+print('alfa'.isnumeric())
+print('Teste-de-quebra-de-string-com-split'.split('-')) #vai quebrar a string onde tem - e retornar uma lista
