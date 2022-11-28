067bda30294951f22895df6fd07f2f1e8968dad6
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2022-11-28 16:11:53 -0300

    type conversions

---
+++ Concepts/cast.py
@@ -0,0 +1,17 @@
+# int conversion
+pontuation_str = '50'
+pontuation = int(pontuation_str)
+print(pontuation)
+print(f'Type: {type(pontuation)}')
+
+# str conversion
+age = 30
+text = 'hello, i\'m ' + str(age) + ' years old'
+print(text)
+print(f'Type: {type(text)}')
+
+# float conversion
+pontation_str = '50'
+pontuation_float = float(pontation_str)
+print(pontuation_float)
+print(f'Type: {type(pontuation_float)}')
