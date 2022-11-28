f0553bda4d186704956d5252b3ffb6117cb4c57c
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2022-11-28 16:13:00 -0300

    change folder

---
+++ Concepts/Strings/name.py
@@ -0,0 +1,5 @@
+name = "ada lovalace"
+name1 = "ADA LOVACE"
+print(name.title())
+print(name.upper())
+print(name1.lower())
---
+++ Concepts/Strings/rstrip.py
@@ -0,0 +1,4 @@
+fav_lang = ' python '
+print(fav_lang.rstrip())
+print(fav_lang.lstrip())
+print(fav_lang.strip())
