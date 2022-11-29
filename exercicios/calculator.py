ed29fd5c64378f94664b74b98ea7f8ab3ad77861
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2022-11-28 21:32:48 -0300

    calculator

---
+++ exercicios/calculator.py
@@ -0,0 +1,17 @@
+num1 = int(input('enter with number 1: '))
+num2 = int(input('enter with number 2: '))
+
+oparator = input('enter with oparator + or - or * or /: ')
+
+
+if(oparator == '+'):
+    print(num1 + num2)
+
+elif(oparator == '-'):
+    print(num1 - num2)
+
+elif(oparator == '*'):
+    print(num1 * num2)
+
+elif(oparator == '/'):
+    print(num1 / num2)
