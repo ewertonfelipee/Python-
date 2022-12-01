2f114fba0fadef3190c44bf33bf4f241083ca43a
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2022-11-30 22:23:29 -0300

    add comments

--- Concepts/looops2.py
+++ Concepts/looops2.py
@@ -14,5 +14,10 @@ while (contador < 1024):
 	print (contador)
 	contador = contador + 1
 '''
+# range parameters
+# 1-inicio da sequencia
+# 2-ultimo da sequencia
+#	ultimo numero - 1
+# 3-de quanto em quanto a sequencia sera incrementada
 for i in range(0, 256, 2):
 	print(i)
