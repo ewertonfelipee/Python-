55c963b12f2a47891065d48cbe5a1c21599b8b26
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2022-12-07 21:05:25 -0300

    cadastro e notebook

--- Concepts/Dict/exemplo1.py
+++ Concepts/Dict/exemplo1.py
@@ -1,3 +1,28 @@
-dictionary = {"nome" : "Ewerton"}
-print(dictionary["nome"])
-#continuar da aula 38
+# dictionary = {"nome" : "Ewerton"}
+# print(dictionary["nome"])
+
+cadastro = {
+    'id' : 1,
+    'nome' : 'ozzy',
+    'raça' : ['shi-tzu','puddle'],
+    'idade' : 3,
+    'pelos' : [
+        {
+            'cor' : 'amarelados',
+            'tamanho' : 'médio'
+        }
+    ]
+}
+
+# print(cadastro['id'])
+# print(cadastro['nome'])
+# print(cadastro['raça'][0])
+# print(cadastro['raça'][1])
+# print(cadastro['idade'])
+# print(cadastro['pelos'])
+# print(cadastro['pelos'][0]['cor'])
+# print(cadastro['pelos'][0]['tamanho'])
+
+raça = cadastro.get('raça', None)
+# None serve para tratar um erro caso uma chave que não existe seja passada
+print(raça)
