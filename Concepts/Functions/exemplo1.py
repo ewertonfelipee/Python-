dba33521f68b878120a5e9d23d79e90e316de504
Ewerton F S Marinho <53940331+ewertonfelipee@users.noreply.github.com>
2022-12-20 15:15:12 -0300

    Function concepts

---
+++ Concepts/Functions/exemplo1.py
@@ -0,0 +1,8 @@
+def apresentation(name, age, role):
+    print(f'Hello, my name is {name}, i\'m {age} years old and working with {role}')
+
+
+apresentation('Ewerton', 24, 'Pentester')
+apresentation('Nathaly', 22, 'Front-End Dev')
+apresentation('Beth', 50, 'Cook')
+
---
+++ Concepts/Functions/exemplo2.py
@@ -0,0 +1,24 @@
+# def args_function(arg1, arg2, *args):
+#     print(arg1)
+#     print(arg2)
+#     print(args)
+
+# args_function(5, 6, 6.5, 'hi')
+
+def soma(maximum, *numbers):
+    result = 0
+
+    numbers_sum = []
+
+    for number in numbers:
+
+        if(result + number) > maximum:
+            break
+
+        result += number
+        numbers_sum.append(number)
+
+
+    return result
+
+print(soma(10,2,3,4,5))
